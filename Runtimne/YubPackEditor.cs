ff4eb0caf192da83338207cb04f3e8a4b7767940
Yuasa Akito <91645837+Yubunen@users.noreply.github.com>
2022-05-11 02:09:43 +0900

    directory update

---
+++ Runtimne/YubPackEditor.cs
@@ -0,0 +1,9 @@
+public class MyClass {
+    private int n;
+    
+    public MyClass() { }
+    public MyClass(int n) { this.n = n; }
+
+    public int getN() { return n; }
+    public void setN(int n) { this.n = n; }
+}
